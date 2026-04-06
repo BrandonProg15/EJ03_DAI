6d34fff2e194c87bb766c432cc153bffb33d6812
Android Studio <devandroid@ort>
2026-04-06 09:57:42 -0300

    hecho hasta el ej06

---
+++ EJ03.js
@@ -0,0 +1,14 @@
+import dayjs from 'dayjs';
+
+
+function mostrarFechaYHora() {
+    const ahora = dayjs();
+
+    const fecha = ahora.format('DD/MM/YYYY');
+    const hora = ahora.format('HH:mm');
+
+    console.log("Fecha actual:", fecha);
+    console.log("Hora actual:", hora);
+}
+
+mostrarFechaYHora();
